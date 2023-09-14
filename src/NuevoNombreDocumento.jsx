558df676daea4c3e52ecbf3379afb6a7dea62bae
giselaC27 <gisela30.10.13@gmail.com>
2023-09-14 18:10:57 -0500

    Se aagrega diseño

--- src/NuevoNombreDocumento.jsx
+++ src/NuevoNombreDocumento.jsx
@@ -21,7 +21,7 @@ function NuevoNombreDocumento() {
       ""
     )}`;
     setNuevoNombre(nuevoNombreGenerado);
-    setCopiado(false); 
+    setCopiado(false);
   };
 
   const copiarAlPortapapeles = () => {
@@ -43,69 +43,90 @@ function NuevoNombreDocumento() {
   };
 
   return (
-    <div className="p-4 mx-auto bg-white rounded">
-    <h1 className="text-3xl font-bold text-center">
-      Generador de Nuevo Nombre de Documento
-    </h1>
-    <div className="p-4 max-w-md mx-auto bg-white rounded shadow-md">
-     
-
-      <div className="mb-4">
-        <label className="block mb-2 font-bold">Código del Documento:</label>
-        <input
-          type="text"
-          value={codigoDocumento}
-          onChange={(e) => setCodigoDocumento(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 font-bold">Versión del Documento:</label>
-        <input
-          type="text"
-          value={versionDocumento}
-          onChange={(e) => setVersionDocumento(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 font-bold">Nombre del Documento:</label>
-        <input
-          type="text"
-          value={nombreDocumento}
-          onChange={(e) => setNombreDocumento(e.target.value)}
-          className="w-full p-2 border rounded"
-        />
-      </div>
-      <button
-        onClick={generarNuevoNombre}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 font-bold"
+    <div className="bg-celeste-pastel min-h-screen custom-shape-divider-top-1694732587 ">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
       >
-        Generar Nuevo Nombre
-      </button>
-      <div className="mt-4">
-        {nuevoNombre && (
-          <div className="flex items-center">
-            <strong className="mr-2">Nuevo Nombre Generado:</strong>
-            <span>{nuevoNombre}</span>
-            <button
-              onClick={copiarAlPortapapeles}
-              className={`ml-2 p-2 text-blue-500 ${
-                copiado ? "text-green-500" : "hover:text-blue-600"
-              }`}
-            >
-              <AiOutlineCopy size={20} />
-            </button>
-          </div>
-        )}
+        <path
+          d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+          opacity=".25"
+          className="shape-fill"
+        ></path>
+        <path
+          d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+          opacity=".5"
+          className="shape-fill"
+        ></path>
+        <path
+          d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+          className="shape-fill"
+        ></path>
+      </svg>
+      <br />
+      <h1 className="text-3xl font-bold text-center ">
+        Generador de Nuevo Nombre de Documento
+      </h1>
+      <br /> <br />
+      <div className="bg-white bg-opacity-80 backdrop-blur-md p-4 max-w-md mx-auto rounded-lg shadow-md">
+        <div className="mb-4">
+          <label className="block mb-2 font-bold">Código del Documento:</label>
+          <input
+            type="text"
+            value={codigoDocumento}
+            onChange={(e) => setCodigoDocumento(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 font-bold">Versión del Documento:</label>
+          <input
+            type="text"
+            value={versionDocumento}
+            onChange={(e) => setVersionDocumento(e.target.value)}
+            className="w-full p-2 border rounded "
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-2 font-bold">Nombre del Documento:</label>
+          <input
+            type="text"
+            value={nombreDocumento}
+            onChange={(e) => setNombreDocumento(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+        </div>
         <button
-          onClick={resetearCampos}
-          className="mt-4 p-2 bg-gray-300 hover:bg-gray-400 text-gray-700 hover:text-gray-800 rounded"
+          onClick={generarNuevoNombre}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 font-bold"
         >
-          Restablecer
+          Generar Nuevo Nombre
         </button>
+        <div className="mt-4">
+          {nuevoNombre && (
+            <div className="flex items-center">
+              <strong className="mr-2">Nuevo Nombre Generado:</strong>
+              <span>{nuevoNombre}</span>
+              <button
+                onClick={copiarAlPortapapeles}
+                className={`ml-2 p-2 text-blue-500 ${
+                  copiado ? "text-green-500" : "hover:text-blue-600"
+                }`}
+              >
+                <AiOutlineCopy size={20} />
+              </button>
+            </div>
+          )}
+          <button
+            onClick={resetearCampos}
+            className="mt-4 p-2 bg-gray-300 hover:bg-gray-400 text-gray-700 hover:text-gray-800 rounded font-bold"
+          >
+            Restablecer
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
